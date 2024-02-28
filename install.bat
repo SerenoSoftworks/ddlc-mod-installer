33c3096cfea66ea9244c4cf359b0af08f7958715
Sereno <82668939+SerenoV7@users.noreply.github.com>
2024-02-28 21:38:52 +0100

    Added installers

---
+++ install.bat
@@ -0,0 +1,32 @@
+@echo off
+
+echo Creating "mod" folder
+mkdir mod
+echo Put the mod's files inside of the mod folder then hit enter
+pause
+echo Downloading DDLC
+powershell -Command "Invoke-WebRequest -Uri https://qiwi.lol/1bwu4922-ddlc-win.zip -OutFile DDLC.zip"
+echo Downloading rpatool
+powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/shizmob/rpatool/master/rpatool -OutFile rpatool.py"
+echo Unpacking DDLC
+powershell -Command "Expand-Archive DDLC.zip"
+ren DDLC-1.1.1-pc DDLC
+
+echo Extracting DDLC
+python ./rpatool.py -x ./DDLC/game/fonts.rpa -o ./DDLC/game/
+echo Extracted fonts.rpa
+python ./rpatool.py -x ./DDLC/game/scripts.rpa -o ./DDLC/game/
+echo Extracted scripts.rpa
+python ./rpatool.py -x ./DDLC/game/audio.rpa -o ./DDLC/game/
+echo Extracted audio.rpa
+python ./rpatool.py -x ./DDLC/game/images.rpa -o ./DDLC/game/
+echo Extracted images.rpa
+
+mkdir DDLCModded
+xcopy DDLC DDLCModded /E /I
+xcopy mod\*.* DDLCModded\game /E /I
+echo Installed modded game in "DDLCModded"
+
+del DDLC.zip
+rmdir /S /Q DDLC
+echo Finished!
---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Downloading DDLC"
+wget -O "DDLC.zip" https://qiwi.lol/1bwu4922-ddlc-win.zip -q --show-progress
+echo "Downloading RPATool"
+wget -O "rpatool.py" https://raw.githubusercontent.com/shizmob/rpatool/master/rpatool -q --show-progress
+echo "Unpacking DDLC"
+unzip DDLC.zip # Unpack the game
+mv DDLC-1.1.1-pc DDLC
+
+echo "Extracting DDLC"
+python3 ./rpatool.py -x ./DDLC/game/fonts.rpa -o ./DDLC/game/
+echo "Extracted fonts.rpa"
+python3 ./rpatool.py -x ./DDLC/game/scripts.rpa -o ./DDLC/game/
+echo "Extracted scripts.rpa"
+python3 ./rpatool.py -x ./DDLC/game/audio.rpa -o ./DDLC/game/
+echo "Extracted audio.rpa"
+python3 ./rpatool.py -x ./DDLC/game/images.rpa -o ./DDLC/game/
+echo "Extracted images.rpa"
+
+echo "Creating mod folder"
+mkdir mod
+echo "Put the mod's files inside of the mod folder then hit enter"
+read -r
+
+mkdir DDLCModded
+cp -r DDLC/* DDLCModded/
+cp -r mod/* DDLCModded/game/
+echo "Installed modded game in 'DDLCModded'"
+
+chmod -R 777 DDLCModded
+rm rpatool.py
+rm DDLC.zip
+rm -rf DDLC
+echo "Install is complete!"
+echo "Modded game is in the "DDLCModded" folder"
